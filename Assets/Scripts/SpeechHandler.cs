7333b8964b7e7c88fef2f7aebcca6d7317a1c167
David Whitney <david_whitney@brown.edu>
2018-06-02 10:29:23 -0400

    first commit

---
+++ Assets/Scripts/SpeechHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using RosSharp.RosBridgeClient;
+
+public class SpeechHandler : MonoBehaviour, ISpeechHandler {
+  
+    void ISpeechHandler.OnSpeechKeywordRecognized(SpeechEventData eventData) {
+    }
+
+    // Sends the goal position to MoveIt
+    public void Plan() {
+        Debug.Log("Plan");
+        //GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //go.AddComponent<Animation>();
+        //go.AddComponent<MeshFilter>();
+        //go.AddComponent<MeshCollider>();
+        //go.AddComponent<MeshRenderer>();
+    }
+
+    // Tells MoveIt to execute the plan
+    public void Move() {
+        Debug.Log("Execute");
+    }
+
+    public void Activate() {
+        Debug.Log("Activate");
+    }
+}
---
+++ Assets/Scripts/TapForSpheres.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
+
+/// <summary>
+/// This class implements IInputClickHandler to handle the tap gesture.
+/// It increases the scale of the object when tapped.
+/// </summary>
+public class TapForSpheres : MonoBehaviour, IInputClickHandler {
+    public void OnInputClicked(InputClickedEventData eventData) {
+        // Increase the scale of the object just as a response.
+        gameObject.transform.localScale += 0.05f * gameObject.transform.localScale;
+        GameObject s = new GameObject();
+        s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        s.transform.position = new Vector3(this.transform.position.x + 2, this.transform.position.y + 2, this.transform.position.z + 2);
+        s.AddComponent<TwoHandManipulatable>();
+        eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
+    }
+}
